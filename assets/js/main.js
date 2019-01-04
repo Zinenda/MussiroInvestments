9e2074c16df39cc81924184fcff383eedbd9f629
Ivo Zinenda <izinenda@gmail.com>
2019-01-04 15:45:59 +0200

    favicons

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+    $('.slider').slider();
+    $('.modal').modal();
+    $('#mensagem').val('New Text');
+    $('.tooltipped').tooltip();
+    $('.fixed-action-btn').floatingActionButton();
+});
